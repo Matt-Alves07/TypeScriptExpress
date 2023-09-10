4b59549145163be659c6f7337c4708f5a50fdd4d
Matheus Alves <mathalves07.winstore@outlook.com>
2023-09-09 21:37:41 -0300

    Created a route to post products, and another one to return an example of product

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,26 @@
-import express  from "express";
+import express, { NextFunction, Request, Response } from "express";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
     return res.send("Hello from express");
 });
+
+app.post("/api/products", (req, res) => {
+    console.log(req.body);
+
+    return res.send("Product included.");
+});
+
+app.get("/api/products/example", (req: Request, res: Response) => {
+    return res.json({
+        name: "Clothe",
+        price: 9.99,
+        inStock: false
+    })
+})
 
 app.listen(3000, () => {
     console.log("Express working");
