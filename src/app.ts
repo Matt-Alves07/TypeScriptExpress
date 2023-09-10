be00da09bf6183901b6da5412705d787bccd8344
Matheus Alves <mathalves07.winstore@outlook.com>
2023-09-09 21:14:09 -0300

    Created the first route using express

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+import express  from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+    return res.send("Hello from express");
+});
+
+app.listen(3000, () => {
+    console.log("Express working");
+});
