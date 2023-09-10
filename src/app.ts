b23c74af140b4d9c728b3f110611dc952e579601
Matheus Alves <mathalves07.winstore@outlook.com>
2023-09-09 21:51:21 -0300

    Created a route receiving a parameter to return a product by id

--- src/app.ts
+++ src/app.ts
@@ -14,13 +14,22 @@ app.post("/api/products", (req, res) => {
     return res.send("Product included.");
 });
 
+app.get("/api/products/:id", (req: Request, res: Response) => {
+    return req.params.id === "1"
+        ? res.json({ id: 1, name: "Shirt", price: 9.99, inStock: false, sizes: ["P", "M", "G", "GG", "XG"] })
+        : res.send("Product not found.");
+})
+
 app.get("/api/products/example", (req: Request, res: Response) => {
     return res.json({
+        id: 0,
         name: "Clothe",
         price: 9.99,
-        inStock: false
-    })
-})
+        inStock: false,
+        sizes: ["P", "M", "G", "GG", "XG"]
+    });
+});
+
 
 app.listen(3000, () => {
     console.log("Express working");
